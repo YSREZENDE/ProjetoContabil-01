0a25d4e4e29ce7272d1cdf506426095925c27174
Ys <107312818+YSREZENDE@users.noreply.github.com>
2022-09-26 20:07:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ var menu = document.querySelector('nav ul');
 var menuBar = document.querySelector('nav . menu-icon');
 var iconMenu = document.querySelector('nv .menu-icon img');
 
-menuBar.add
+menuBar.addEventListener('click' () {
+
+        menu.classList.toggle('active');
+
+    };
+  
