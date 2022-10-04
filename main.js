797e1344247d122ba811918e39f1782f307d07f9
YSREZENDE <107312818+YSREZENDE@users.noreply.github.com>
2022-10-04 16:01:41 -0300

    04/10/22

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var menu = document.querySelector('nav ul');
-var menuBar = document.querySelector('nav . menu-icon');
+var menuBar = document.querySelector('nav .menu-icon');
 var iconMenu = document.querySelector('nav .menu-icon img');
 
 menuBar.addEventListener('click',function(){
     
-alert("olá mundo");
+alert('teste');
 
 });
+
